039814eb46c3265bd469c85220b55fab53a5751a
Houda <136978213+houda-naimi@users.noreply.github.com>
2026-05-11 11:51:25 +0100

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -1,1 +1,9 @@
+/*
+Script Purpose:
+    This script creates views for the Gold layer of the data warehouse.
+    The Gold layer contains the final fact and dimension tables designed
+    using a Star Schema model.
 
+    Each view transforms and combines data from the Silver layer to deliver
+    clean, enriched, and business-ready datasets for reporting and analytics.
+*/
